10eafc784896452ff3e04ff777ec33b72a6a0dd1
asimovravil <asimovravil06@gmail.com>
2026-04-04 14:34:00 +0500

    Initial Commit

---
+++ Egg Heist/Egg_HeistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Egg_HeistApp.swift
+//  Egg Heist
+//
+//  Created by Ravil on 04.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Egg_HeistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
